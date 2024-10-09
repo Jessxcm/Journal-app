a6883bf6308ce7a8ef51e37f4865ba19ccaf7af0
Jessica <calajess@gmail.com>
2024-10-09 11:04:13 -0500

    adding material

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
+import { ThemeProvider } from '@emotion/react';
+import { AppTheme } from './theme/AppTheme';
 
 const router = createBrowserRouter(AppRouter);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AppTheme>
     <RouterProvider router={router} />
+    </AppTheme>
   </StrictMode>,
 )
